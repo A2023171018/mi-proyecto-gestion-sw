342c239284b4953fb0f946e88a8dfe6d7de04cd2
A2023171018 <contactoangelgabbana@gmail.com>
2026-03-10 00:52:16 -0600

    feat: proyecto inicial

---
+++ app.py
@@ -0,0 +1,14 @@
+def suma(a, b):
+    return a + b
+
+def resta(a, b):
+    return a - b
+
+def division(a, b):
+    if b == 0:
+        return None
+    return a / b
+
+print(suma(5, 3))
+print(resta(10, 4))
+print(division(10, 2))
